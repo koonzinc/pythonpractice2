24e862d3ce20a8b5debf059d210540116603c55e
koonzinc <williamgeorgekoonz@gmail.com>
2022-10-01 18:23:12 +0000

    finished

--- .learn/resets/10-Array-Methods/app.py
+++ .learn/resets/10-Array-Methods/app.py
@@ -1,6 +1,4 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
-def sort_names(arr):
-    print(sorted(arr))
 
-sort_names(names)
+print(sort_names(names))
--- exercises/08-Function-that-returns/app.py
+++ exercises/08-Function-that-returns/app.py
@@ -5,3 +5,6 @@ def euro_to_yen(euro_value):
 	return euro_value * 124.15
 
 ####### ↓ YOUR CODE BELOW ↓ #######
+dollar_value = dollar_to_euro(137)
+dollar_to_yen = euro_to_yen(dollar_value)
+print(dollar_to_yen)
